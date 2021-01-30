f32e7bffbef30138cf2bc721abc562755536fa54
Shellywell123 <benjamin@shellswell.com>
2021-01-30 19:45:00 +0000

    added window icon and name

--- conf.lua
+++ conf.lua
@@ -1,6 +1,7 @@
 function love.conf(t)
     t.version = "11.3"
-    t.window.title = "Fennel Game"
+    t.window.title = "Kafkaesque"
+    t.window.icon = "assets/flower.png"
     t.window.width = 1024
     t.window.height = 768
 
